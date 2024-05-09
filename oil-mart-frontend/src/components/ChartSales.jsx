39f039c2fa001538d467e4a6aaf75864626c741c
Pasindu <pasinduj27@gmail.com>
2024-05-10 01:24:22 +0530

    table styling and dashboard completed

--- oil-mart-frontend/src/components/ChartSales.jsx
+++ oil-mart-frontend/src/components/ChartSales.jsx
@@ -1,99 +1,106 @@
 import { Legend } from "@headlessui/react";
 import React from "react";
-import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
-
-
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import PieChartSupplier from "./PieChartSupplier";
 
 function ChartSales() {
-    const data = [
-        {
-            name: 'Jan',
-            Expense: 4000,
-            Income: 2400
-        },
-        {
-            name: 'Feb',
-            Expense: 3000,
-            Income: 1398
-        },
-        {
-            name: 'Mar',
-            Expense: 2000,
-            Income: 9800
-        },
-        {
-            name: 'Apr',
-            Expense: 2780,
-            Income: 3908
-        },
-        {
-            name: 'May',
-            Expense: 1890,
-            Income: 4800
-        },
-        {
-            name: 'Jun',
-            Expense: 2390,
-            Income: 3800
-        },
-        {
-            name: 'July',
-            Expense: 3490,
-            Income: 4300
-        },
-        {
-            name: 'Aug',
-            Expense: 2000,
-            Income: 9800
-        },
-        {
-            name: 'Sep',
-            Expense: 2780,
-            Income: 3908
-        },
-        {
-            name: 'Oct',
-            Expense: 1890,
-            Income: 4800
-        },
-        {
-            name: 'Nov',
-            Expense: 2390,
-            Income: 3800
-        },
-        {
-            name: 'Dec',
-            Expense: 3490,
-            Income: 4300
-        }
-    ]
+  const data = [
+    {
+      name: "Jan",
+      Expense: 4000,
+      Income: 2400,
+    },
+    {
+      name: "Feb",
+      Expense: 3000,
+      Income: 1398,
+    },
+    {
+      name: "Mar",
+      Expense: 2000,
+      Income: 9800,
+    },
+    {
+      name: "Apr",
+      Expense: 2780,
+      Income: 3908,
+    },
+    {
+      name: "May",
+      Expense: 1890,
+      Income: 4800,
+    },
+    {
+      name: "Jun",
+      Expense: 2390,
+      Income: 3800,
+    },
+    {
+      name: "July",
+      Expense: 3490,
+      Income: 4300,
+    },
+    {
+      name: "Aug",
+      Expense: 2000,
+      Income: 9800,
+    },
+    {
+      name: "Sep",
+      Expense: 2780,
+      Income: 3908,
+    },
+    {
+      name: "Oct",
+      Expense: 1890,
+      Income: 4800,
+    },
+    {
+      name: "Nov",
+      Expense: 2390,
+      Income: 3800,
+    },
+    {
+      name: "Dec",
+      Expense: 3490,
+      Income: 4300,
+    },
+  ];
   return (
     <div className="h-[22rem] bg-white p-4 rounded-sm border-gray-200 flex flex-col flex-1 ">
-        <strong className="text-gray-700 font-medium">Transcations</strong>
-        <div className="w-full mt-3 flex-1 text-xs">
-
-      <ResponsiveContainer width={'99%'} height={300}>
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
+      <strong className="text-gray-700 font-medium ">Transcations</strong>
+      <div className="w-full mt-3 flex-1 text-xs">
+        <ResponsiveContainer width={"100%"} height={'100%'}>
+          <BarChart 
+            width={500}
+            height={300}
+            data={data}
+            margin={{
               top: 20,
               right: 10,
               left: -10,
               bottom: 0,
             }}
-            >
-            <CartesianGrid strokeDasharray="3 3 0 0" vertical={false}/>
-            <XAxis dataKey="name"/>
-            <YAxis/>
-            <Tooltip/>
-            <Legend/>
+          >
+            <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
             <Bar dataKey="Expense" fill="#8884d8" />
             <Bar dataKey="Income" fill="#82ca9d" />
-        </BarChart>
-      </ResponsiveContainer>
-            </div>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+     
     </div>
   );
 }
--- oil-mart-frontend/src/components/Dashboard.jsx
+++ oil-mart-frontend/src/components/Dashboard.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import DashboardStatGrid from "./DashboardStatGrid";
 import ChartSales from "./ChartSales";
+import PieChartSupplier from "./PieChartSupplier";
+import RecentOrders from "./RecentOrders";
+import PopularProducts from "./PopularProducts";
 
 export default function Dashboard() {
   return (
     <div className="flex gap-4 flex-col">
-       <DashboardStatGrid/>
-       <ChartSales/>
+      <DashboardStatGrid />
+      <div className="flex flex-row gap-4 w-full">
+        <ChartSales></ChartSales>
+        <PieChartSupplier />
+      </div>
+      <div className="flex flex-row gap-4 w-full">
+        <RecentOrders/>
+        <PopularProducts/>
+      </div>
     </div>
   );
 }
